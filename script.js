fc944a54ce18b593bcb3680a3cbc3694d12428a9
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-08-19 09:44:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,56 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+
+//progress filled
+progressBar.addEventListener('timeupdate', updateProgress);
+function updateProgress() {
+    const progress = (video.currentTime / video.duration) * 100;
+    video.style.width = `${progress}%`;
+}
+
+//play pause buttons
+toggle.addEventListener('click', playPauseVideo);
+function playPauseVideo() {
+    if(video.paused) {
+        video.play();
+    }else video.pause();
+}
+
+video.addEventListener('play', changeButton);
+video.addEventListener('pause', changeButton);
+function changeButton() {
+    toggle.textContent = video.paused ? "►" : "❚ ❚";   
+}
+
+//volume change
+ranges[0].addEventListener('input', updateVolume);
+function updateVolume() {
+    video.volume = ranges[0].value;
+}
+
+
+//playback speed
+ranges[1].addEventListener('click', updateSpeed);
+function updateSpeed() {
+    video.playbackRate = ranges[1].value;
+}
+
+
+//skip buttons
+skipButtons.forEach(skipButton => {
+    skipButton.addEventListener('click', skipVideo);
+});
+
+function skipVideo() {
+    let timeSkip = parseFloat(this.dataset.skip);
+    video.currentTime += timeSkip;
+}
+
+
+// skipButtons.forEach(button => button.addEventListener('click', skipTime));
+// function skipTime() {
+//   const skipTime = parseFloat(this.dataset.skip);
+//   video.currentTime += skipTime;
+// }
